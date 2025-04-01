74ada50027cff1822e09a03d9421189b8c8d2971
samuelg <117109324+samuelgaravito@users.noreply.github.com>
2025-03-31 20:11:00 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+base:"https://bluexAgency.github.io/wowmedia360-landing"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
